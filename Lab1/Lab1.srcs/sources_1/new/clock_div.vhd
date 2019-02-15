feaeb32034440ac5b915ea1600fc19282775670c
mam1010 <38589307+mam1010@users.noreply.github.com>
2019-02-14 20:47:48 -0500

    Add files via upload

---
+++ Lab1/Lab1.srcs/sources_1/new/clock_div.vhd
@@ -0,0 +1,54 @@
+----------------------------------------------------------------------------------
+-- Company: 
+-- Engineer: 
+-- 
+-- Create Date: 02/14/2019 08:21:22 PM
+-- Design Name: 
+-- Module Name: clock_div - Behavioral
+-- Project Name: 
+-- Target Devices: 
+-- Tool Versions: 
+-- Description: 
+-- 
+-- Dependencies: 
+-- 
+-- Revision:
+-- Revision 0.01 - File Created
+-- Additional Comments:
+-- 
+----------------------------------------------------------------------------------
+
+
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+use IEEE.NUMERIC_STD.all;
+
+-- Uncomment the following library declaration if using
+-- arithmetic functions with Signed or Unsigned values
+--use IEEE.NUMERIC_STD.ALL;
+
+-- Uncomment the following library declaration if instantiating
+-- any Xilinx leaf cells in this code.
+--library UNISIM;
+--use UNISIM.VComponents.all;
+
+entity clock_div is
+    Port ( CLK_IN : in STD_LOGIC;
+           CLK_OUT : out STD_LOGIC);
+end clock_div;
+
+architecture Behavioral of clock_div is
+    signal counter : std_logic_vector(26 downto 0) := (others => '0');
+begin
+    process (CLK_IN)
+    begin
+        if(rising_edge(CLK_IN)) then
+            if (unsigned(counter) < 124999999) then
+               counter <= std_logic_vector(unsigned(counter) + 1);
+            else
+               counter <= (others => '0');
+            end if;
+        end if;
+    end process;
+
+end Behavioral;
